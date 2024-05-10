318cca673191a98395d1ed18c24b9c33c572bfe2
Zosia Samosia <Zosia@Samosia.pl>
2024-05-10 13:41:27 +0200

    add hamburger menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+document.addEventListener("click", () => {
+  const button = document.querySelector(".material-icons");
+  button.classList.toggle("active");
+
+  if (button.classList.contains("active")) {
+    button.textContent = "close";
+  } else {
+    button.textContent = "menu";
+  }
+});
